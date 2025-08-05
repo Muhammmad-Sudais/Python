c55f00b724ec5d1615025279c8d8cf9fad823caa
Muhammad Sudais <muhammadsoodais@gmail.com>
2025-08-05 16:19:19 -0700

    uploaded

---
+++ datatype_in_python.py
@@ -0,0 +1,22 @@
+# datatypes in python
+# there are alot of datatypes in python some of them are given below
+
+# interger
+
+a = 3
+print("the variable type is: ",a)
+
+
+#float
+
+b = 5.0
+print("the variable type is: ",b)
+
+
+# boolean
+
+a = 5>4
+print(a)
+print(type(a))
+
+
